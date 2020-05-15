ef94088631cb198a558b123a276d4240e11f0a0e
PranavBP <bellam.pranav@gmail.com>
2020-05-15 23:52:17 +0530

    Updates

---
+++ precache-manifest.e056b6d468b9c0b8a74716f70a2902a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0384f815f88afde2b64fa30d28fd04a4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "870c8686d021a1519880",
+    "url": "/my-app/static/css/main.b5f9e209.chunk.css"
+  },
+  {
+    "revision": "33d0d88723b79f1f1f69",
+    "url": "/my-app/static/js/2.3f1357ed.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/my-app/static/js/2.3f1357ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870c8686d021a1519880",
+    "url": "/my-app/static/js/main.7a729a0e.chunk.js"
+  },
+  {
+    "revision": "883cd556cdfcda25bd63",
+    "url": "/my-app/static/js/runtime-main.fd1cb725.js"
+  }
+]);
