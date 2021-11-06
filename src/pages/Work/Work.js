7d2c7e598a028a6b772c5c080443356a41f27be2
Bp, Pranav <pranav.bp@rakuten.com>
2021-11-06 16:03:26 +0530

    fixes education

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -16,7 +16,7 @@ const Work = ({ user }) => {
                 <JobTitle>{work.company}</JobTitle> <span>{work.location}</span>
                 <span> &sdot; </span>
                 <span>
-                  {work.start.year} to {work.end.year}
+                  {work.start.month}-{work.start.year} to {work.end.month}-{work.end.year}
                 </span>
               </div>
               <Paragraph>{work.summary}</Paragraph>
